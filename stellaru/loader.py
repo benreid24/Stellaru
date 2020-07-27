85696e6da825ad1f7597c624cfc8f1811a28ab21
Ben Reid <reidben24@gmail.com>
2020-07-27 00:49:30 -0400

    Remove example from code. Add actual snapshot. Clean up economy

--- stellaru/loader.py
+++ stellaru/loader.py
@@ -71,177 +71,6 @@ ECONOMY_CLASSES = {
     'country_base': [EC_BASE]
 }
 
-EXAMPLE_SNAPSHOT = {
-    'name': 'The Raviscidian Ravagers',
-    'active_empires': 8,
-    'edict_count': 14,
-    'sprawl': 1500,
-    'unity': {
-        'output': 1000,
-        'traditions': 40,
-        'ascension_perks': 8,
-    },
-    'systems': {
-        'surveyed': 600,
-        'owned': 100,
-        'starbases': 29
-    },
-    'wars': {
-        'total': 3,
-        'participating': 1
-    },
-    'federation': {
-        'name': '',
-        'members': 0,
-        'cohesion': 0,
-        'xp': 0,
-        'level': 0,
-        'leader': False
-    },
-    'leaders': {
-        'total': 18,
-        'scientist': 5,
-        'general': 0,
-        'governor': 1,
-        'admiral': 12,
-        'avg_age': 140,
-        'max_age': 288
-    },
-    'standing': {
-        'victory_rank': 1,
-        'military_power': 4000000,
-        'tech_power': 31000,
-        'economy_power': 31000
-    },
-    'construction': {
-        'queue_count': 200,
-        'queued_items': 400,
-        'avg_queue size': 2,
-        'max_queue_size': 100,
-        # TODO - type breakdown?
-    },
-    'economy': {
-        'resource name': {
-            'stockpile': 10000,
-            'net': 200,
-            'income': {
-                'total': 1000,
-                'starbases': {
-                    'total': 200,
-                    'base': 50,
-                    'modules': 50,
-                    'buildings': 50,
-                    'defense': 50 # if possible
-                },
-                'stations': 50, # mining/research
-                'ships': {
-                    'total': 400,
-                    'base': 200,
-                    'components': 200
-                },
-                'armies': 50,
-                'megastructures': 100,
-                'planets': {
-                    'total': 200,
-                    'robots': 0,
-                    'districts': 50,
-                    'buildings': 100,
-                    'jobs': {
-                        'total': 50,
-                        'job1': 25,
-                        'job2': 25,
-                        'etc': 0
-                    }
-                }
-            },
-            'expense': {
-                'same breakdown': 'as income'
-            }
-        }
-    },
-    'tech': {
-        'research': {
-            'society': 200,
-            'engineering': 200,
-            'physics': 200
-        },
-        'completed': {
-            'total': 200,
-            'society': ['list', 'of techs'],
-            'engineering': ['list', 'of techs'],
-            'physics': ['list', 'of techs']
-        },
-        'available': {
-            'total': 50,
-            'society': ['list', 'of techs'],
-            'engineering': ['list', 'of techs'],
-            'physics': ['list', 'of techs']
-        }
-    },
-    'planets': {
-        'total': 30,
-        'total_districts': 350,
-        'planets': [
-            {
-                'name': 'Admin World',
-                'size': 32,
-                'type': 'Savannah'
-            }
-        ]
-    },
-    'pops': {
-        'total': 1800,
-        'species': {
-            'species1': 900,
-            'species2': 900
-        },
-        'jobs': {
-            'job1': 900,
-            'job2': 900
-        }
-    },
-    'fleets': {
-        'strength': 4000000,
-        'ships': {
-            'total': 202,
-            'corvette': 100,
-            'destroyers': 60,
-            'cruisers': 20,
-            'battlehips': 18,
-            'titans': 2,
-            'colossus': 1,
-            'juggernaut': 1
-        },
-        'fleets': [
-            {
-                'name': 'Primary Task Force',
-                'strength': 300000,
-                'ships': {
-                    'total': 200,
-                    'corvette': 100,
-                    'destroyers': 60,
-                    'cruisers': 20,
-                    'battlehips': 18,
-                    'titans': 2
-                }
-            }
-        ]
-    },
-    'armies': {
-        'total': 400,
-        'strength': 80000,
-        'defense': {
-            'total': 350,
-            'strength': 40000,
-             # TODO - planet breakdown? strongest?
-        },
-        'assault': {
-            'total': 50,
-            'strength': 40000
-        }
-    }
-}
-
 
 def get_empires(state):
     return {
@@ -454,19 +283,21 @@ def _build_resource_breakdown(budget):
         for resource, amount in resources.items():
             if resource not in breakdown:
                 breakdown[resource] = {
-                    'total': amount
+                    'total': amount,
+                    'breakdown': {}
                 }
             else:
                 breakdown[resource]['total'] += amount
-            rb = breakdown[resource]
+            rb = breakdown[resource]['breakdown']
             for class_name in classes:
                 if class_name not in rb:
                     rb[class_name] = {
-                        'total': amount
+                        'total': amount,
+                        'breakdown': {}
                     }
                 else:
                     rb[class_name]['total'] += amount
-                rb = rb[class_name]
+                rb = rb[class_name]['breakdown']
     return breakdown
 
 
@@ -474,10 +305,20 @@ def _get_economy(state, empire):
     resources = state['country'][empire]['modules']['standard_economy_module']['resources']
     resource_names = [name for name in resources.keys()]
     budgets = state['country'][empire]['budget']['current_month']
+    income = _build_resource_breakdown(budgets['income'])
+    spending = _build_resource_breakdown(budgets['expenses'])
+    nets = {}
+    for resource in income.keys():
+        net = income[resource]['total']
+        if resource in spending:
+            net -= spending[resource]['total']
+        nets[resource] = net
+
     return {
         'stockpile': resources,
-        'income': _build_resource_breakdown(budgets['income']),
-        'spending': _build_resource_breakdown(budgets['expenses'])
+        'net_income': nets,
+        'income': income,
+        'spending': spending
     }
 
 
