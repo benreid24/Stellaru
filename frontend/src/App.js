d87b7c73b8b5c90b30f55bf6c8bb74c3ba7b2113
Ben Reid <reidben24@gmail.com>
2020-07-31 19:52:13 -0400

    Start some component skeletons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import {createMuiTheme} from '@material-ui/core'
+import {ThemeProvider} from '@material-ui/core'
+
 import './App.css';
+import SaveChooser from './SaveChooser/SaveChooser';
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <SaveChooser></SaveChooser>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/SaveChooser/SaveChooser.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import SaveCard from './SaveCard';
+
+class SaveChooser extends React.Component {
+    constructor(props) {
+        super(props);
+        // TODO
+    }
+
+    render() {
+        return (
+            <SaveCard></SaveCard>
+        );
+    }
+}
+
+export default SaveChooser;
