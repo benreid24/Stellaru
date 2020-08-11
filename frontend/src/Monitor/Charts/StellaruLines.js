f7bb55c6c155eefb15164a579835c4f283d4e4a5
Ben Reid <reidben24@gmail.com>
2020-08-10 23:39:11 -0400

    Switch to Victory charts

--- frontend/src/Monitor/Charts/StellaruLines.js
+++ frontend/src/Monitor/Charts/StellaruLines.js
@@ -1,33 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {LineChart, CartesianGrid, XAxis, Label, YAxis, Line, ResponsiveContainer} from 'recharts';
+import {VictoryLine} from 'victory';
+import {Chart} from './Charts';
 
 import './Charts.css';
 
 function StellaruLines(props) {
-    const data = [
-        {label: 'point 1', x: 1, y: 5},
-        {label: 'point 2', x: 2, y: 7},
-        {label: 'point 3', x: 3, y: 12},
-        {label: 'point 4', x: 4, y: 25},
-        {label: 'point 5', x: 5, y: 59},
-    ];
+    const [data, setData] = useState([
+        {x: 1, y: 5},
+        {x: 2, y: 7},
+        {x: 3, y: 12},
+        {x: 4, y: 25},
+        {x: 5, y: 59},
+    ]);
 
     return (
-        <div className='chart'>
-            <ResponsiveContainer width='100%' height={300}>
-                <LineChart data={data} syncId='what?'>
-                    <CartesianGrid stroke='#bfbfbf' fill='#323232'/>
-                    <XAxis type='number' dataKey='x' height={40} stroke='#dfdfdf'>
-                        <Label value='X' position='insideBottom' fill='#dfdfdf'/>
-                    </XAxis>
-                    <YAxis type='number' dataKey='y' width={80} stroke='#dfdfdf'>
-                        <Label value='Y' position='outside' fill='#dfdfdf'/>
-                    </YAxis>
-                    <Line key='myline' type='monotone' stroke='#cc7722' dataKey='y'/>
-                </LineChart>
-            </ResponsiveContainer>
-        </div>
+        <Chart width='100%' height={300}>
+            <VictoryLine style={{data: {stroke: '#aa5522'}}} data={data}/>
+        </Chart>
     );
 }
 
