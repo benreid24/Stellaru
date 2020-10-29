9507074987b4a68ff54c8fd52494d01a5a1aad8c
Ben Reid <reidben24@gmail.com>
2020-10-28 23:51:02 -0400

    Update help tab

--- frontend/src/Monitor/Tabs/Help.js
+++ frontend/src/Monitor/Tabs/Help.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {translate} from '../../Translator';
+
 function Help(props) {
     const connInfo = props.connInfo ? props.connInfo : {};
     const localIp = connInfo.local_ip ? connInfo.local_ip : '?';
@@ -11,23 +13,23 @@ function Help(props) {
 
     return (
         <div className='monitorTab helpPage'>
-            <h2 className='helpTitle'>Multiplayer</h2>
-            <h3 className='helpSubtitle'>Connection Info</h3>
-            <p className='helpLabel'>Local IP: <span className='helpValue'>{localIp}</span></p>
-            <p className='helpLabel'>Stellaru Port: <span className='helpValue'>{port}</span></p>
-            <p className='helpLabel'>LAN Link: <a href={lanLink}>{lanLink}</a></p>
-            <p className='helpLabel'>Internet Link: <a href={internetLink}>{internetLink}</a></p>
-            <h3 className='helpSubtitle'>Instructions</h3>
+            <h2 className='helpTitle'>{translate('Multiplayer')}</h2>
+            <h3 className='helpSubtitle'>{translate('Connection Info')}</h3>
+            <p className='helpLabel'>{translate('Local IP')}: <span className='helpValue'>{localIp}</span></p>
+            <p className='helpLabel'>{translate('Stellaru Port')}: <span className='helpValue'>{port}</span></p>
+            <p className='helpLabel'>{translate('LAN Link')}: <a href={lanLink}>{lanLink}</a></p>
+            <p className='helpLabel'>{translate('Internet Link')}: <a href={internetLink}>{internetLink}</a></p>
+            <h3 className='helpSubtitle'>{translate('Instructions')}</h3>
             <ol>
                 <li>
-                    If your friends are not on your local network forward the port 42069 to your machine:
+                    {translate('If your friends are not on your local network forward the port 42069 to your machine')}:
                     <ol>
-                        <li>Navigate to your routers administration page. Likely something like <a href='192.168.1.1'>http://192.168.1.1</a></li>
-                        <li>Find the port forwarding page</li>
-                        <li>Forward external port <b>42069</b> to the Local IP and Stellaru Port (internal port) from above</li>
+                        <li>{translate('Navigate to your routers administration page. Likely something like')} <a href='192.168.1.1'>http://192.168.1.1</a></li>
+                        <li>{translate('Find the port forwarding page')}</li>
+                        <li>{translate('Forward external port')} <b>42069</b> {translate('to the Local IP and Stellaru Port (internal port) from above')}</li>
                     </ol>
                 </li>
-                <li>Give your friends the LAN Link if they are on your network, or the Internet Link if they are not</li>
+                <li>{translate('Give your friends the LAN Link if they are on your network, or the Internet Link if they are not')}</li>
             </ol>
         </div>
     )
