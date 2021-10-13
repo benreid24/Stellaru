dcfcda824f516c49339d502aabbf721759fc4478
Ben Reid <reidben24@gmail.com>
2021-10-13 10:59:26 -0400

    Switch to typescript config. Add empty leaderboard tab. Build out tenative leaderboard context

---
+++ frontend/src/Monitor/Tabs/Leaderboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Leaderboard(props) {
+    const data = props.data;
+
+    return (
+        <div className='monitorTab'>
+            <div className='row chartRow'>
+                Leaderboard here
+            </div>
+        </div>
+    );
+}
+
+export default Leaderboard;
