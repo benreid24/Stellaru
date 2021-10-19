f32f2aaf215139d4c4285cdb943d69679bd7549b
Akuukis <akuukis@kalvis.lv>
2021-10-19 21:32:15 +0300

    feat(leaderboard): add Victory chart

--- frontend/src/Monitor/Tabs/Leaderboard.js
+++ frontend/src/Monitor/Tabs/Leaderboard.js
@@ -4,6 +4,7 @@ import {FleetPowerChart} from '../Leaderboard/Charts/FleetPower';
 import {GDPChart} from '../Leaderboard/Charts/GDP';
 import {EmpireSize} from '../Leaderboard/Charts/EmpireSize';
 import {StatusBoard} from '../Leaderboard/StatusBoard';
+import { VictoryChart } from 'Monitor/Leaderboard/Charts/Victory';
 
 function Leaderboard(props) {
     const data = props.data;
@@ -23,6 +24,9 @@ function Leaderboard(props) {
                 </div>
                 <div className='col-xl-6 col-lg-6 col-md-12 mb-3 leaderboardChart'>
                     <EmpireSize data={data}/>
+                </div>
+                <div className='col-xl-6 col-lg-6 col-md-12 mb-3 leaderboardChart'>
+                    <VictoryChart data={data}/>
                 </div>
             </div>
         </div>
