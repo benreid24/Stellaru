7592ce2720188c0b4f67b2a124cef9726dfdaa99
Ben Reid <reidben24@gmail.com>
2021-10-19 16:24:29 -0400

    Implement pop/systems/planets leaderboard chart

---
+++ frontend/src/Monitor/Leaderboard/Charts/EmpireSize.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import Chart from 'Monitor/Charts/Chart';
+import LineChart from 'Monitor/Charts/LineChart';
+import {selectNested} from 'Monitor/Charts/Util';
+import {useLeaderboardContext} from '../Context';
+import {getTimeseries, sumReducer} from '../Selectors';
+import {registerChart} from 'Monitor/Charts/ChartRegistry';
+import {LeaderboardChartProps} from './Types';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import {makeStyles} from '@material-ui/core/styles';
+
+type ChartedValue = 'pops' | 'planets' | 'systems';
+const Labels: Record<ChartedValue, string> = {
+    'pops': 'Total Pops',
+    'planets': 'Total Colonies',
+    'systems': 'Total Systems'
+}
+
+const Name = 'Aggregate Size';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+}));
+
+export const EmpireSize: React.FC<LeaderboardChartProps> = ({data, name: n, overlay}) => {
+    const classes = useStyles();
+
+    const name = n ? n : Name;
+    const {groupState, filterState} = useLeaderboardContext();
+
+    const [chartedValue, setChartedValue] = React.useState<ChartedValue>('systems');
+    const onChartChange = (event: any) => setChartedValue(event.target.value);
+
+    const selector = (snap: any, eid: number) => {
+        switch (chartedValue) {
+            case 'systems':
+                return selectNested(`leaderboard/empire_summaries/${eid}/system_count`, snap);
+            case 'planets':
+                return selectNested(`leaderboard/empire_summaries/${eid}/planets/count`, snap);
+            case 'pops':
+                return selectNested(`leaderboard/empire_summaries/${eid}/pop_count`, snap);
+            default:
+                return 0;
+        }
+    };
+
+    const series = getTimeseries(data, groupState, filterState, selector, sumReducer);
+
+    return (
+        <Chart name={name} title={Name} titleColor='#6666cd' overlay={overlay}>
+            <div className='leaderboardChartForm'>
+                <FormControl className={classes.formControl}>
+                    <Select value={chartedValue} onChange={onChartChange}>
+                        <MenuItem value='systems'>{Labels['systems']}</MenuItem>
+                        <MenuItem value='planets'>{Labels['planets']}</MenuItem>
+                        <MenuItem value='pops'>{Labels['pops']}</MenuItem>
+                    </Select>
+                </FormControl>
+            </div>
+            <div className='leaderboardFormChartContent'>
+                <LineChart
+                    name={name}
+                    data={data}
+                    lines={series.map(gts => gts.timeseries)}
+                    allowIsolation={false}
+                    yAxisLabel={Labels[chartedValue]}
+                />
+            </div>
+        </Chart>
+    );
+}
+
+registerChart(
+    Name,
+    'Compare empire or federation populations, colony counts, and number of systems',
+    EmpireSize,
+    'Leaderboard'
+)
--- frontend/src/Monitor/Tabs/Leaderboard.js
+++ frontend/src/Monitor/Tabs/Leaderboard.js
@@ -2,23 +2,27 @@ import React from 'react';
 import {TechProgressChart} from '../Leaderboard/Charts/TechProgress';
 import {FleetPowerChart} from '../Leaderboard/Charts/FleetPower';
 import {GDPChart} from '../Leaderboard/Charts/GDP';
+import {EmpireSize} from '../Leaderboard/Charts/EmpireSize';
 import {StatusBoard} from '../Leaderboard/StatusBoard';
 
 function Leaderboard(props) {
     const data = props.data;
 
     return (
-        <div className='customTab'>
+        <div className='leaderboardTab'>
             <StatusBoard data={data}/>
             <div className='row chartRow'>
-            <div className='col-xl-6 col-lg-6 col-md-12 mb-3 presetChart'>
+                <div className='col-xl-6 col-lg-6 col-md-12 mb-3 leaderboardChart'>
                     <FleetPowerChart data={data}/>
                 </div>
-                <div className='col-xl-6 col-lg-6 col-md-12 mb-3 presetChart'>
+                <div className='col-xl-6 col-lg-6 col-md-12 mb-3 leaderboardChart'>
                     <TechProgressChart data={data}/>
                 </div>
-                <div className='col-xl-6 col-lg-6 col-md-12 mb-3 presetChart'>
+                <div className='col-xl-6 col-lg-6 col-md-12 mb-3 leaderboardChart'>
                     <GDPChart data={data}/>
+                </div>
+                <div className='col-xl-6 col-lg-6 col-md-12 mb-3 leaderboardChart'>
+                    <EmpireSize data={data}/>
                 </div>
             </div>
         </div>
